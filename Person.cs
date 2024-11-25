73d335667f645428c90cc4d7d8da560fe0dc7386
awilsMookie <jermal@swbell.net>
2024-11-25 10:34:52 -0600

    Initial upload
    Initial creation

---
+++ Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RazorWithComponent
+{
+	public class Person
+	{
+		public int Id { get; set; }	
+		public string Name { get; set; } = string.Empty;
+		public string Designation { get; set; } = string.Empty;
+		public DateOnly DOJ { get; set; }
+		public bool IsActive { get; set; }
+	}
+}
